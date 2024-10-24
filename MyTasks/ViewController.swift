1066544218c4d905fcb007aa87930b1f2835bd57
Alexandre Henrique da Silva <echo.alexandre@gmail.com>
2024-10-24 18:24:04 -0300

    Initial Commit

---
+++ MyTasks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTasks
+//
+//  Created by Alexandre Henrique da Silva on 24/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
